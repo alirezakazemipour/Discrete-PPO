4e27e25e99c657fc3a2dc617a821c8d1c2e55cb4
Alireza <kazemipour.alireza@gmail.com>
2020-12-14 22:37:28 +0330

    Clean the code.

---
+++ Common/logger.py
@@ -0,0 +1,125 @@
+import time
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+import torch
+import os
+import datetime
+import glob
+from collections import deque
+
+
+class Logger:
+    def __init__(self, brain, **config):
+        self.config = config
+        self.brain = brain
+        self.log_dir = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        self.start_time = 0
+        self.duration = 0
+        self.episode = 0
+        self.episode_reward = 0
+        self.running_reward = 0
+        self.running_act_prob = 0
+        self.position = 0
+        self.running_position = 0
+        self.max_episode_reward = -np.inf
+        self.moving_avg_window = 10
+        self.running_training_logs = 0
+        self.moving_weights = np.repeat(1.0, self.moving_avg_window) / self.moving_avg_window
+        self.last_10_ep_rewards = deque(maxlen=10)
+        self.running_last_10_r = 0  # It is not correct but does not matter.
+
+        if self.config["do_test"] and self.config["train_from_scratch"]:
+            self.create_wights_folder()
+            self.log_params()
+
+        self.exp_avg = lambda x, y: 0.9 * x + 0.1 * y if (y != 0).all() else y
+
+    def create_wights_folder(self):
+        if not os.path.exists("Models"):
+            os.mkdir("Models")
+        os.mkdir("Models/" + self.log_dir)
+
+    def log_params(self):
+        with SummaryWriter("Logs/" + self.log_dir) as writer:
+            for k, v in self.config.items():
+                writer.add_text(k, str(v))
+
+    def on(self):
+        self.start_time = time.time()
+
+    def off(self):
+        self.duration = time.time() - self.start_time
+
+    def log_iteration(self, *args):
+        iteration, training_logs, action_prob = args
+
+        self.running_act_prob = self.exp_avg(self.running_act_prob, action_prob)
+        self.running_training_logs = self.exp_avg(self.running_training_logs, np.array(training_logs))
+
+        if iteration % (self.config["interval"] // 3) == 0:
+            self.save_params(self.episode, iteration)
+
+        with SummaryWriter("Logs/" + self.log_dir) as writer:
+            writer.add_scalar("Episode Reward", self.episode_reward, self.episode)
+            writer.add_scalar("Running Episode Reward", self.running_reward, self.episode)
+            writer.add_scalar("Position", self.position, self.episode)
+            writer.add_scalar("Running last 10 Reward", self.running_last_10_r, self.episode)
+            writer.add_scalar("Max Episode Ext Reward", self.max_episode_reward, self.episode)
+            writer.add_scalar("Running Action Probability", self.running_act_prob, iteration)
+            writer.add_scalar("Running Position", self.running_position, iteration)
+            writer.add_scalar("Running PG Loss", self.running_training_logs[0], iteration)
+            writer.add_scalar("Running Value Loss", self.running_training_logs[1], iteration)
+            writer.add_scalar("Running Entropy", self.running_training_logs[2], iteration)
+            writer.add_scalar("Running Explained variance", self.running_training_logs[3], iteration)
+
+        self.off()
+        if iteration % self.config["interval"] == 0:
+            print("Iter: {}| "
+                  "E: {}| "
+                  "E_Reward: {}| "
+                  "E_Running_Reward: {:.3f}| "
+                  "Position: {}| "
+                  "Running Position: {}"
+                  "Iter_Duration: {:.3f}| "
+                  "Time: {} "
+                  .format(iteration,
+                          self.episode,
+                          self.episode_reward,
+                          self.running_reward,
+                          self.position,
+                          self.running_position,
+                          self.duration,
+                          datetime.datetime.now().strftime("%H:%M:%S"),
+                          )
+                  )
+        self.on()
+
+    def log_episode(self, *args):
+        self.episode, self.episode_reward, self.position = args
+
+        self.max_episode_reward = max(self.max_episode_reward, self.episode_reward)
+
+        self.running_reward = self.exp_avg(self.running_reward, self.episode_reward)
+        self.running_position = self.exp_avg(self.running_position, self.position)
+
+        self.last_10_ep_rewards.append(self.episode_reward)
+        if len(self.last_10_ep_rewards) == self.moving_avg_window:
+            self.running_last_10_r = np.convolve(self.last_10_ep_rewards, self.moving_weights, 'valid')
+
+    def save_params(self, episode, iteration):
+        torch.save({"current_policy_state_dict": self.brain.current_policy.state_dict(),
+                    "optimizer_state_dict": self.brain.optimizer.state_dict(),
+                    "scheduler_state_dict": self.brain.scheduler.state_dict(),
+                    "iteration": iteration,
+                    "episode": episode,
+                    "running_reward": self.running_reward,
+                    "position": self.position
+                    },
+                   "Models/" + self.log_dir + "/params.pth")
+
+    def load_weights(self):
+        model_dir = glob.glob("Models/*")
+        model_dir.sort()
+        checkpoint = torch.load(model_dir[-1] + "/params.pth")
+        self.log_dir = model_dir[-1].split(os.sep)[-1]
+        return checkpoint
--- main.py
+++ main.py
@@ -1,56 +1,52 @@
-from runner import Worker
+from Common.runner import Worker
+from Common.play import Play
+from Common.config import get_params
+from Common.logger import Logger
 from torch.multiprocessing import Process, Pipe
 import numpy as np
-from numpy import asarray
-from brain import Brain
+from Brain.brain import Brain
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from tqdm import tqdm
-import time
-from torch.utils.tensorboard import SummaryWriter
-from play import Play
 
-env_name = "SuperMarioBros-1-1-v0"
-test_env = gym_super_mario_bros.make(env_name)
-test_env = JoypadSpace(test_env, SIMPLE_MOVEMENT)
-n_actions = test_env.action_space.n
-n_workers = 2
-state_shape = (4, 84, 84)
-device = "cuda"
-iterations = 24000
-log_period = 10
-T = 128
-epochs = 3
-lr = 2.5e-4
-clip_range = 0.1
-LOAD_FROM_CKP = False
-Train = True
+# n_actions = test_env.action_space.n
 
 
 def run_workers(worker, conn):
     worker.step(conn)
 
 
-def receive(p):
-    return p.recv()
-
-
-def send_action(p, a):
-    p.send(a)
-
-
 if __name__ == '__main__':
-    brain = Brain(state_shape, n_actions, device, n_workers, epochs, iterations, clip_range, lr)
-    if Train:
-        if LOAD_FROM_CKP:
-            running_reward, init_iteration, episode = brain.load_params()
+    config = get_params()
+
+    config.update({"env_name": "SuperMarioBros-" + str(config['world'])+ "-" + str(config['stage']) + "-v0"})
+    test_env = gym_super_mario_bros.make(config["env_name"])
+    test_env = JoypadSpace(test_env, SIMPLE_MOVEMENT)
+    config.update({"n_actions": test_env.action_space.n})
+    test_env.close()
+
+    brain = Brain(**config)
+    logger = Logger(brain, **config)
+
+    if config["do_test"]:
+        if not config["train_from_scratch"]:
+            checkpoint = logger.load_weights()
+            brain.set_from_checkpoint(checkpoint)
+            running_reward = checkpoint["running_reward"]
+            init_iteration = checkpoint["iteration"]
+            episode = checkpoint["episode"]
+            position = checkpoint["visited_rooms"]
+            logger.running_reward = running_reward
+            logger.episode = episode
+            logger.position = position
         else:
             init_iteration = 0
-            running_reward = 0
+            running__reward = 0
             episode = 0
+            position = 0
 
-        workers = [Worker(i, state_shape, env_name) for i in range(n_workers)]
+        workers = [Worker(i, **config) for i in range(config["n_workers"])]
 
         parents = []
         for worker in workers:
@@ -59,88 +55,70 @@ if __name__ == '__main__':
             parents.append(parent_conn)
             p.start()
 
-        init_total_states = np.zeros((n_workers, T,) + state_shape)
-        init_total_actions = np.zeros((n_workers, T))
-        init_total_rewards = np.zeros((n_workers, T))
-        init_total_dones = np.zeros((n_workers, T), dtype=np.bool)
-        init_total_values = np.zeros((n_workers, T))
-        init_total_log_probs = np.zeros((n_workers, T))
-        init_next_states = np.zeros((n_workers,) + state_shape, dtype=np.uint8)
-        init_next_values = np.zeros(n_workers)
+        rollout_base_shape = config["n_workers"], config["rollout_length"]
 
+        init_states = np.zeros(rollout_base_shape + config["state_shape"], dtype=np.uint8)
+        init_actions = np.zeros(rollout_base_shape, dtype=np.uint8)
+        init_action_probs = np.zeros(rollout_base_shape + (config["n_actions"],))
+        init_rewards = np.zeros(rollout_base_shape)
+        init_dones = np.zeros(rollout_base_shape, dtype=np.bool)
+        init_values = np.zeros(rollout_base_shape)
+        init_log_probs = np.zeros(rollout_base_shape)
+        init_next_states = np.zeros((rollout_base_shape[0],) + config["state_shape"], dtype=np.uint8)
+
+        logger.on()
         episode_reward = 0
-        for iteration in tqdm(range(init_iteration + 1, iterations + 1)):
-            start_time = time.time()
-            total_states = init_total_states
-            total_actions = init_total_actions
-            total_rewards = init_total_rewards
-            total_dones = init_total_dones
-            total_values = init_total_values
-            total_log_probs = init_total_log_probs
+        concatenate = np.concatenate
+        for iteration in tqdm(range(init_iteration + 1, config["total_iterations"] + 1)):
+            total_states = init_states
+            total_actions = init_actions
+            total_action_probs = init_action_probs
+            total_rewards = init_rewards
+            total_dones = init_dones
+            total_values = init_values
+            total_log_probs = init_log_probs
             next_states = init_next_states
-            next_values = init_next_values
 
-            for t in range(T):
-                # for worker_id, parent in enumerate(parents):
-                #     s = parent.recv()
-                #     total_states[worker_id, t] = s
-                total_states[:, t] = asarray(list(map(receive, parents)), dtype=np.uint8)
+            for t in range(config["rollout_length"]):
+                for worker_id, parent in enumerate(parents):
+                    s = parent.recv()
+                    total_states[worker_id, t] = s
 
-                total_actions[:, t], total_values[:, t], total_log_probs[:, t] = \
+                total_actions[:, t], total_values[:, t], total_log_probs[:, t], total_action_probs[:, t] =\
                     brain.get_actions_and_values(total_states[:, t], batch=True)
+                for parent, a in zip(parents, total_actions[:, t]):
+                    parent.send(a)
 
-                # for parent, a in zip(parents, total_actions[:, t]):
-                #     parent.send(int(a))
-                list(map(send_action, parents, total_actions[:, t]))
-
-                # for worker_id, parent in enumerate(parents):
-                x = list(map(receive, parents))
-                s_, r, d = asarray(x, dtype=object)[:, 0], asarray(x, dtype=object)[:, 1], asarray(x, dtype=object)[:, 2]
-                total_rewards[:, t] = r
-                total_dones[:, t] = d
-                next_states[:] = np.concatenate(s_).reshape(n_workers, *state_shape)
+                infos = []
+                for worker_id, parent in enumerate(parents):
+                    s_, r, d, info = parent.recv()
+                    infos.append(info)
+                    total_rewards[worker_id, t] = r
+                    total_dones[worker_id, t] = d
+                    next_states[worker_id] = s_
 
                 episode_reward += total_rewards[0, t]
                 if total_dones[0, t]:
                     episode += 1
-                    if episode == 1:
-                        running_reward = episode_reward
-                    else:
-                        running_reward = 0.99 * running_reward + 0.01 * episode_reward
-                    episode_reward = 0
+                    position = infos[0]["x_pos"]
+                    logger.log_episode(episode, episode_reward, position)
+                    episode_ext_reward = 0
 
-            _, next_values, _ = brain.get_actions_and_values(next_states, batch=True)
+                    _, next_values, *_ = brain.get_actions_and_values(next_states, batch=True)
 
-            total_states = total_states.reshape((n_workers * T,) + state_shape)
-            total_actions = total_actions.reshape(n_workers * T)
-            total_log_probs = total_log_probs.reshape(n_workers * T)
+                    training_logs = brain.train(states=concatenate(total_states),
+                                                actions=concatenate(total_actions),
+                                                rewards=total_rewards,
+                                                dones=total_dones,
+                                                values=total_values,
+                                                log_probs=concatenate(total_log_probs),
+                                                next_values=next_values)
 
-            # Calculates if value function is a good predictor of the returns (ev > 1)
-            # or if it's just worse than predicting nothing (ev =< 0)
-            total_loss, entropy, ev = brain.train(total_states, total_actions, total_rewards,
-                                                  total_dones, total_values, total_log_probs, next_values)
-            brain.schedule_lr()
-            brain.schedule_clip_range(iteration)
+                    logger.log_iteration(iteration,
+                                         training_logs,
+                                         total_action_probs[0].max(-1).mean())
 
-            if iteration % log_period == 0:
-                print(f"Iter: {iteration}| "
-                      f"Episode: {episode}| "
-                      f"Ep_reward: {episode_reward:.1f}| "
-                      f"Running_reward: {running_reward:.1f}| "
-                      f"Total_loss: {total_loss:.3f}| "
-                      f"Explained variance:{ev:.3f}| "
-                      f"Entropy: {entropy:.3f}| "
-                      f"Iter_duration: {time.time() - start_time:.3f}| "
-                      f"Lr: {brain.scheduler.get_last_lr()}| "
-                      f"Clip_range:{brain.epsilon:.3f}")
-                brain.save_params(iteration, running_reward, episode)
-
-            with SummaryWriter(env_name + "/logs") as writer:
-                writer.add_scalar("running reward", running_reward, iteration)
-                writer.add_scalar("episode reward", episode_reward, iteration)
-                writer.add_scalar("explained variance", ev, iteration)
-                writer.add_scalar("loss", total_loss, iteration)
-                writer.add_scalar("entropy", entropy, iteration)
     else:
-        play = Play(env_name, brain)
+        checkpoint = logger.load_weights()
+        play = Play(config["env_name"], brain, checkpoint)
         play.evaluate()
